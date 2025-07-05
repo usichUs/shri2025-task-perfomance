3e8c64246a80ee25966bb0f30a3ba4c6bd79ec80
Nikita Usachev <guyfoks737@gmail.com>
2025-07-05 14:25:06 +0300

    migrate to react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./reset.css";
+import "./styles.css";
+
+import { Header, Main } from "./Page";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
+
+export default App;
